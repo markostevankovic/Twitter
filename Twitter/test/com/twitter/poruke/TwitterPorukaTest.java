f420890a1f14356b0924046243ed3f4ffe0a5efa
Marko Stevankovic <markostevankovic95@gmail.com>
2016-03-30 20:50:00 +0200

    Napravljeni paketu u test source folderu

--- Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
+++ Twitter/test/com/twitter/poruke/TwitterPorukaTest.java
@@ -1,4 +1,4 @@
-package com.twitter;
+package com.twitter.poruke;
 
 import static org.junit.Assert.*;
 
@@ -7,8 +7,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.twitter.poruke.TwitterPoruka;
 
 public class TwitterPorukaTest 
 {
