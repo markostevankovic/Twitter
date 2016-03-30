eeff97b37d8e5972407a7ac3e91e69517638e608
Marko Stevankovic <markostevankovic95@gmail.com>
2016-03-30 18:15:13 +0200

    Napravljena  JavaDoc dokumentacija za klasu TwitterPoruka, njene atribute i metode

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,15 +1,46 @@
 package com.twitter.poruke;
 
+/**
+ * Klasa TwitterPoruka predstavlja status korisnika na drustvenoj mrezi Twitter.
+ * Svaki twit se sastoji od korisnickog imena i poruke tj statusa koja/koji
+ * je objavljen.
+ * 
+ * @author Marko Stevankovic
+ *
+ */
+
 public class TwitterPoruka 
 {
+	/*
+	 * Atribut koji predstavlja korisnicko ime, tj korisnika
+	 * koji objavljuje status/poruku.
+	 */
 	private String korisnik;
+	
+	/*
+	 * Atribut koji predstavlja samu poruku, tj. sadrzaj poruke/statusa
+	 * koji se objavljuje.
+	 */
 	private String poruka;
 	
+	/*
+	 * Metoda koja vraca naziv korisnickog naloga sa kog se objavljuje status/poruka
+	 * 
+	 * @return String
+	 */
 	public String getKorisnik() 
 	{
 		return korisnik;
 	}
 	
+	/*
+	 * Metoda koja postavlja naziv korisnickog naloga.
+	 * 
+	 * @param korisnik predstavlja naziv na koji se treba postaviti korisnicki nalog.
+	 * 
+	 * @throws RuntimeException ako je uneti parametar null ili prazan string
+	 * 
+	 */
 	public void setKorisnik(String korisnik)
 	{
 		if (korisnik==null || korisnik == "")
@@ -17,11 +48,24 @@ public class TwitterPoruka
 		
 		this.korisnik = korisnik;
 	}
+	
+	/*
+	 * Metoda koja vraca sadrzaj objavljenje poruke/statusa
+	 * 
+	 * @return String
+	 */
 	public String getPoruka() 
 	{
 		return "poruka";
 	}
 	
+	/*
+	 * Metoda koja postavlja sadrzaj statusa/poruke na zeljeni tekst.
+	 * 
+	 * @param poruka sadrzaj same poruke/statusa.
+	 * 
+	 * @throws RuntimeException ako je sadrzaj parametra null ili string duzine vece od 140 karaktera
+	 */
 	public void setPoruka(String poruka) 
 	{
 		if (poruka==null || this.poruka.length()>140)
@@ -29,6 +73,11 @@ public class TwitterPoruka
 		this.poruka = poruka;
 	}
 	
+	/*
+	 * Redefinisana metoda toString() klase Object
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString()
 	{
 		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
